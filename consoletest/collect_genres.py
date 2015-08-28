8c97cd53e0466a9e971d03f3d5e33259d3099441
Bao Vuong <vqbao@lv5.org>
2015-08-28 13:47:05 -0400

    added collect_genres.py

---
+++ consoletest/collect_genres.py
@@ -0,0 +1,10 @@
+
+class MusicGenre:
+    def __init__(self,genre,summary,wiki_link):
+        self.genre = genre
+        self.summary = summary
+        self.wiki_link = wiki_link
+        self.subgenres = []
+        self.fusion_genres = []
+        self.derivative_forms = []
+        self.stylistic_origins = []
