84650c3ead78763478ccd6ee8cd23a52a5c40b39
Bao Vuong <vqbao@lv5.org>
2015-09-02 17:50:15 -0400

    figuring out a way to extract genre info box

--- consoletest/collect_genres.py
+++ consoletest/collect_genres.py
@@ -1,6 +1,9 @@
+import codecs
 import httplib
 import json
+import locale
 import sqlite3
+import sys
 import urllib
 import wikipedia
 
@@ -15,16 +18,24 @@ class MusicGenre:
         self.stylistic_origins = []
 
 def get_wiki_content(page_title):
-	arguments = {}
-	arguments['action'] = 'query'
-	arguments['titles'] = page_title
-	arguments['prop'] = 'revisions'
-	arguments['rvprop'] = 'content'
-	arguments['format'] = 'json'
-	
-	url = 'https://en.wikipedia.org/w/api.php?%s' % (urllib.urlencode(arguments))
-	
-	return json.loads(urllib.urlopen(url).read())
+    arguments = {}
+    arguments['action'] = 'query'
+    arguments['titles'] = page_title
+    arguments['prop'] = 'revisions'
+    arguments['rvprop'] = 'content'
+    arguments['format'] = 'json'
+    url = 'https://en.wikipedia.org/w/api.php?%s' % (urllib.urlencode(arguments))
+    
+    return json.loads(urllib.urlopen(url).read())
+
+def extract_genre_info_box(text):
+
+    # probably going to use a PDA type of thing
+    enclosing_brackets = [] # the stack 
+
+
+
+sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
 content = get_wiki_content('House_music')
 print content['query']['pages'][content['query']['pages'].keys()[0]]['revisions'][0]['*']
