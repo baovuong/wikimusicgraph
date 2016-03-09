eb13831aadd7b2e30238a1b29e15ebc5b53cdd5b
Bao Vuong <vqbao@lv5.org>
2016-03-08 23:24:09 -0800

    starting infobox genre extraction

--- consoletest/collect_genres.py
+++ consoletest/collect_genres.py
@@ -56,7 +56,7 @@ def extract_genre_info_box(text):
         'default': [ (),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(21,'a',''),(),(),() ],
     }
     state_stack = ['Z']
-    
+
     for character in text:
 
         step = state_transitions[character][current_state] if character in state_transitions else state_transitions['default'][current_state]
@@ -65,13 +65,13 @@ def extract_genre_info_box(text):
             current_state = 0
             state_stack = ['Z']
             output = ''
-            
+
         elif step[1] != state_stack[-1]:
             # dead. try again
             current_state = 0
             state_stack = ['Z']
             output = ''
-        
+
         else:
             # do the step
             #print state_stack, current_state
@@ -83,7 +83,7 @@ def extract_genre_info_box(text):
                     state_stack.pop()
                 else:
                     state_stack.append(step[2])
-        #print output 
+        #print output
         if state_stack[-1] == 'Z' and current_state == 21:
             #print 'done'
             break
@@ -92,7 +92,154 @@ def extract_genre_info_box(text):
 
 def infobox_to_music_genre(infobox_text):
     # I will probably do the same thing. just FSM my way to this. technically the most efficient route
-    pass 
+    genre_name = ''
+    assignment = {
+        'stylistic_origins': [],
+        'subgenres': [],
+        'derivative_forms': [],
+        'fusion_genres': []
+    }
+    state = 0
+    transitions = [
+    ]
+
+    for character in infobox_text:
+        if state == 0:
+            if character == '|':
+                state = 1
+            continue
+        if (state == 1):
+            if character == 'f':
+                state = 40
+            elif character == 'd':
+                state = 29
+            elif character == 's':
+                state = 7
+            elif character == 'n':
+                state = 2
+            else:
+                state = 0
+            continue
+        if (state == 2):
+            continue
+        if (state == 3):
+            continue
+        if (state == 4):
+            continue
+        if (state == 5):
+            continue
+        if (state == 6):
+            continue
+        if (state == 7):
+            continue
+        if (state == 8):
+            continue
+        if (state == 9):
+            continue
+        if (state == 10):
+            continue
+        if (state == 11):
+            continue
+        if (state == 12):
+            continue
+        if (state == 13):
+            continue
+        if (state == 14):
+            continue
+        if (state == 15):
+            continue
+        if (state == 16):
+            continue
+        if (state == 17):
+            continue
+        if (state == 18):
+            continue
+        if (state == 19):
+            continue
+        if (state == 20):
+            continue
+        if (state == 21):
+            continue
+        if (state == 22):
+            continue
+        if (state == 23):
+            continue
+        if (state == 24):
+            continue
+        if (state == 25):
+            continue
+        if (state == 26):
+            continue
+        if (state == 27):
+            continue
+        if (state == 28):
+            continue
+        if (state == 29):
+            continue
+        if (state == 30):
+            continue
+        if (state == 31):
+            continue
+        if (state == 32):
+            continue
+        if (state == 33):
+            continue
+        if (state == 34):
+            continue
+        if (state == 35):
+            continue
+        if (state == 36):
+            continue
+        if (state == 37):
+            continue
+        if (state == 38):
+            continue
+        if (state == 39):
+            continue
+        if (state == 40):
+            continue
+        if (state == 41):
+            continue
+        if (state == 42):
+            continue
+        if (state == 43):
+            continue
+        if (state == 44):
+            continue
+        if (state == 45):
+            continue
+        if (state == 46):
+            continue
+        if (state == 47):
+            continue
+        if (state == 48):
+            continue
+        if (state == 49):
+            continue
+        if (state == 50):
+            continue
+        if (state == 51):
+            continue
+        if (state == 52):
+            continue
+        if (state == 53):
+            continue
+        if (state == 54):
+            continue
+        if (state == 55):
+            continue
+        if (state == 56):
+            continue
+        if (state == 57):
+            continue
+        if (state == 58):
+            continue
+        if (state == 59):
+            continue
+        if (state == 60):
+            continue
+        if (state == 61):
+            continue
 
 sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
